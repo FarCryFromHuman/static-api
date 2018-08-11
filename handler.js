49125fb7f86049b8cde02c0dede0ea11526555d6
FarCryFromHuman <unsurety@gmail.com>
2018-08-11 07:13:31 -0600

    Added readme, root json directory, try-parse, and better error messaging

--- handler.js
+++ handler.js
@@ -1,32 +1,39 @@
 const fs = require('fs');
+const jsonTryParse = require('json-try-parse');
 
 const handleRequest = (req, res) => {
     let method = req.method.toLowerCase();
-    let safePath = getActualPath(req.path.slice(1))
-    if (!safePath) {
-        res.status(404).send('Response for ' + req.path + ' is not present, and no fallbacks were available.');
+    let safePath = getActualPath(`static-json${req.path}`)
+    if (!safePath.successful) {
+        res.status(404).send(`Response for '${req.path}' is not present, and no fallbacks were available.`);
         return;
     }
 
-    let json = getStaticData(safePath);
-    if (!checkStaticData(json, method)) {
-        res.status(404).send('Response file ' + safePath + ' does not have a response for ' + method);
+    let json = getStaticData(safePath.path);
+    if (!json.successful) {
+        res.status(404).send(`Response file '${safePath.path}' is not valid JSON.`);
+        return;
+    }
+    console.debug(json.body);
+
+    if (!checkStaticData(json.body, method)) {
+        res.status(404).send(`Response file '${safePath.path}' does not have a response for '${method}'.`);
         return;
     }
 
-    res.status(json[method].statusCode).json(json[method].body);
+    res.status(json.body[method].statusCode).json(json.body[method].body);
 }
 
 const getActualPath = route => {
     let tokens = route.split('/');
 
     let path = tokens.slice(0, tokens.length - 1).reduce(getActualFolder, []);
-    if (!path) return undefined;
+    if (!path) return { successful: false, path: undefined };
 
     let file = getActualFile(path, tokens[tokens.length - 1]);
-    if (!file) return undefined;
+    if (!file) return { successful: false, path: undefined };
 
-    return [...path, file].join('/');
+    return { successful: true, path: [...path, file].join('/') };
 }
 
 const getActualFolder = (path, token) =>
@@ -39,18 +46,20 @@ const getActualFolder = (path, token) =>
                 : undefined;
 
 const getActualFile = (path, file) =>
-    fs.existsSync([...path, file + '.json'].join('/'))
+    fs.existsSync([...path, `${file}.json`].join('/'))
         ? file + '.json'
         : fs.existsSync([...path, '_.json'].join('/'))
             ? '_.json'
             : undefined;
 
-const checkStaticData = (json, verb) =>
-    json.hasOwnProperty(verb)
-    && json[verb].hasOwnProperty('statusCode')
-    && json[verb].hasOwnProperty('body');
+const checkStaticData = (json, method) =>
+    json.hasOwnProperty(method)
+    && json[method].hasOwnProperty('statusCode')
+    && json[method].hasOwnProperty('body');
 
-const getStaticData = path =>
-    JSON.parse(fs.readFileSync(path));
+const getStaticData = path => {
+    let json = jsonTryParse(fs.readFileSync(path));
+    return { successful: !!json, body: json };
+}
 
 exports.handler = handleRequest;
---
+++ readme.md
@@ -0,0 +1,31 @@
+# How static data works
+* For any given request, the request route is mapped to the hierarchy under the static-json folder.
+* Routes begin with '/', so '/{myResource}/{myIdenifier}' will map directly to 'static-json/{myResource}/{myIdentifier}.json'.
+* Fallbacks are available; any token in the route that cannot map directly will attempt to map to a folder/file called '_'.
+* For example, '/myResource' will attempt to map to 'static-json/myResouce.json', and if it cannot, will attempt to map to 'static-json/_.json'.
+* This works for folders as well; for example, '/people/1234/hobbies/hockey' will map to:
+  * If available, 'static-json/people/1234/hobbies/hockey.json'
+  * Then, 'static-json/_/1234/hobbies/hockey.json'
+  * Then, 'static-json/(people | _)/(1234 | _)/hobbies/hockey.json'
+  * And on, ending with the most desperate attempt to match, 'static-json/_/_/_/_.json'
+  * The tokenization is progressive, so any given token may be a fallback without breaking the hierarchy.
+* Once a valid JSON file is found for the request route, the server looks for a valid entry matching the request method
+* Valid JSON response files should match this format:
+`{
+`    "method": {
+`        "statusCode": httpStatusCode,
+`        "body" : {
+`            // some data
+`        }
+`    }
+`}
+  * __method__ should be a valid HTTP method, spelled in lowercase (get, post, put, etc.).
+  * __httpStatusCode__ should be a valid HTTP response status code (200, 404, 500, etc.).
+  * __body__ should contain the raw JSON you desire as a response.
+
+# Unsupported but potential future features
+* Fallbacks for methods
+* Pattern matching to choose responses based on:
+  * Headers and header values
+  * Query string parameters
+  * Request body content (for appropriate methods)
